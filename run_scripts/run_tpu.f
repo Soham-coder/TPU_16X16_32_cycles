5a0057e91420044a7ae4893481fdca84c67ee876
Soham-coder <mindalsarama@gmail.com>
2023-05-27 20:45:53 +0530

    tpu

--- run_scripts/run_tpu.f
+++ run_scripts/run_tpu.f
@@ -1,5 +1,23 @@
-+incdir+"."
++incdir+"."+"../RTL/"+"../RTL/control/"+"../RTL/control/master_control/"+"../RTL/accumTable/"+"../RTL/ip/"+"../RTL/ip/dsp_multiplier/"+"../RTL/input_mem/"+"../RTL/output_mem/"+"../RTL/memory/"+"../RTL/mmu/"+"../RTL/reluArr/"+"../RTL/weightFifo/"
 
-TPU.sv
+/* top soc whole code
+../top_soc.sv
+../tb_top_soc.sv
+*/
+
+../RTL/weightFifo/dff8.v
+../RTL/weightFifo/weightFifo.v
+
+../RTL/mmu/pe.v
+../RTL/mmu/sysArrRow.v
+../RTL/mmu/sysArr.v
+
+../RTL/memory/memArr.v
+../RTL/ip/input_mem/input_mem.v
+../RTL/ip/input_mem/altsyncram.v
+
+../TESTBENCH/tb_tpu.sv
+
+
 
 +access+rw
